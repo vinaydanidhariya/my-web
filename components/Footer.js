581d3e3a437de0758628a9a6f63e02b76fac97f7
vinay danidhariya <vinaydanidhariya4114@gmail.com>
2023-04-25 20:34:26 +0530

    old web

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+	const Footer = document.getElementById("Footer");
+	const Container = document.createElement("div");
+	Container.classList.add("container");
+	Container.innerHTML = `
+			
+  `;
+	Footer.appendChild(Container);
+}
+Footer();
